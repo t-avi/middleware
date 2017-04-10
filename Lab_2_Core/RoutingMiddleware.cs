5c80ea4c8eb5aa07d747d0f8c579e584212bf043
t_avi <taniaiv.95@gmail.com>
2017-04-10 22:29:55 +0500

    Middleware task

---
+++ Lab_2_Core/RoutingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Lab_2_Core
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path == "/index")
+            {
+                var link = File.ReadAllText(@"D:\Таня\Lab_2_Core\Lab_2_Core\wwwroot\Index.html");
+                await context.Response.WriteAsync(link);
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("<h1>About</h1> <br> <a href='/index'>Back to homepage</a>");
+            }
+            else if (path == "/contact")
+            {
+                await context.Response.WriteAsync("<h1>Contact</h1> <br> <a href='/index'>Back to homepage</a>");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+    }
+}
--- Lab_2_Core/Startup.cs
+++ Lab_2_Core/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,26 +9,14 @@ namespace Lab_2_Core
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+       public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            app.UseMiddleware<RoutingMiddleware>();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
